99a87e0427ceb89602cd72639d99a91b325259f6
Anjany Pandey <anjany.pandey06@gmail.com>
2025-07-02 23:48:03 +0530

    feat: set full height for main container in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="flex-center">
+    <div className="flex-center h-[100vh]">
       <h1 className="text-3xl text-indigo-300">Hello GSAP</h1>
     </div>
   );
