7209d220e3fbd510f781f32c0c88d6ae511c60ae
Anjany Pandey <anjany.pandey06@gmail.com>
2025-07-03 12:08:22 +0530

    feat: add Cocktails component with GSAP animations for parallax effect

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { cocktailLists, mockTailLists } from "../../constants";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const Cocktails = () => {
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#cocktails",
+        start: "top 30%", // start the animation when the top of the cocktails section hits 30% of the viewport height
+        end: "bottom 80%", // end the animation when the bottom of the cocktails section hits 80% of the viewport height
+        scrub: true, // smooth scrubbing
+      },
+    });
+
+    parallaxTimeline
+      .from("#c-left-leaf", {
+        x: -100,
+        y: 100,
+      })
+      .from("#c-right-leaf", {
+        x: 100,
+        y: 100,
+      });
+  });
+  return (
+    <section id="cocktails" className="noisy">
+      <img
+        src="/images/cocktail-left-leaf.png"
+        alt="left-leaf"
+        id="c-left-leaf"
+      />
+      <img
+        src="/images/cocktail-right-leaf.png"
+        alt="right-leaf"
+        id="c-right-leaf"
+      />
+      <div className="list">
+        <div className="popular">
+          <h2>Most Popular Cocktails: </h2>
+
+          <ul>
+            {cocktailLists.map((drink) => (
+              <li key={drink.name}>
+                <div className="md:me-28">
+                  <h3>{drink.name}</h3>
+                  <p>
+                    {drink.country} | {drink.detail}
+                  </p>
+                </div>
+                <span>- {drink.price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="loved">
+          <h2>Most Popular Cocktails: </h2>
+
+          <ul>
+            {mockTailLists.map((drink) => (
+              <li key={drink.name}>
+                <div className="me-28">
+                  <h3>{drink.name}</h3>
+                  <p>
+                    {drink.country} | {drink.detail}
+                  </p>
+                </div>
+                <span>- {drink.price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cocktails;
